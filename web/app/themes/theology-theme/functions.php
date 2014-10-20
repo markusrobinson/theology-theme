aa74a889f8ebb1ebc5e1f08a9eb59eb09f40b7d1
Markus Robinson <markus.robinson@gmail.com>
2014-10-20 18:55:17 -0400

    removing old plugins. adding new core plugin.

--- web/app/themes/theology-theme/functions.php
+++ web/app/themes/theology-theme/functions.php
@@ -21,9 +21,6 @@ $roots_includes = array(
   'lib/gallery.php',         // Custom [gallery] modifications
   'lib/comments.php',        // Custom comments modifications
   'lib/scripts.php',         // Scripts and stylesheets
-  'lib/admin.php',           // Admin Scripts and stylesheets
-  //'lib/extras/the-events-calendar/the-events-calendar.php',          // Custom functions
-  //'lib/extras/seriously-simple-podcasting/seriously-simple-podcasting.php',          // Custom functions
 );
 
 foreach ($roots_includes as $file) {
--- web/app/themes/theology-theme/lib/extras.php
+++ web/app/themes/theology-theme/lib/extras.php
@@ -20,36 +20,3 @@ function roots_wp_title($title) {
   return $title;
 }
 add_filter('wp_title', 'roots_wp_title', 10);
-
-
-function metaboxes() {
-
-    $meta_boxes['mighty_metabox_video'] = array(
-        'id'         => 'mighty-metabox-video',
-        'title'      => __( 'Video Details', 'cmb' ),
-        'pages'      => array( 'post' ),
-        'context'    => 'normal',
-        'priority'   => 'high',
-        'show_names' => true,
-        'fields'     => array(
-            array(
-                'name' => __( 'Self Hosted Video URL', 'cmb' ),
-                'desc' => __( 'Enter in the URL to the video MP4 or M4V file. (Example: http://domain.com/movie.mp4)', 'cmb' ),
-                'id'   => 'video-url',
-                'type' => 'text_url',
-            ),
-            array(
-                'name' => __( 'Third Party Video', 'cmb' ),
-                'desc' => __( 'Enter a YouTube, Vimeo, etc URL. Supports services listed at <a href="http://codex.wordpress.org/Embeds">http://codex.wordpress.org/Embeds</a>.', 'cmb' ),
-                'id'   => 'video-embed',
-                'type' => 'oembed',
-            ),
-        ),
-    );
-
-
-
-    return $meta_boxes;
-}
-
-add_action( 'init', 'metaboxes', 9999 );
